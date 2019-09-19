cd0aae2b87794de4f773b52ecbd2858483a63ce9
William Chrapcynski <wchrapcynski@gmail.com>
2019-09-18 21:34:29 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,15 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardsInPlay[0])
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[1])
+if(cardsInPlay.length === 2) {
+  if(cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+}
